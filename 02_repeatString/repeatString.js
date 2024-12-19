752642a63bb567233d49a86395cdb447a13d0a96
Kenneth Aidan Lee <158255466+kennethalee@users.noreply.github.com>
2024-12-19 21:37:54 +0900

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return 'ERROR'
+    }
 
+    res = ''
+    // loop times time and append to res
+    for (let i = 0; i < times; i++) {
+        res += word;
+    }
+    return res;
 };
 
 // Do not edit below this line
